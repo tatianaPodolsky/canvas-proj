8e712a0e9ca6f7bbc2dbe3c4613e4c3eacf952de
Tatiana Podolsky <podolskaya.tatiana@gmail.com>
2019-02-13 15:28:29 +0200

    merge

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-function canvasClicked(ev) {
+var gCanvas;
+var gCtx;
+var gShapes = [
+    {name:'pen', func: drawPoint}, {name: 'rect', func: drawRect}, {name: 'circle', func: drawCircle}]
+function init() {
+    gCanvas = document.querySelector('#canvas-container');
+    gCtx = gCanvas.getContext('2d');
+}
+
+function canvasClicked(ev, shape) {
     console.log('hi, ev: ', ev);
+    ev.offsetX
+
+}
+
+
+function drawPoint(x, y, color) {
+    gCtx.beginPath()
+    gCtx.arc(100, 300, 50, 0, 2 * Math.PI);
+    gCtx.stroke()
+    gCtx.fill()
 }
